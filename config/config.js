f00fe4b0db47b16f812481c746c438e21133c098
shraddha-timbadiya <112844472+shraddha-timbadiya@users.noreply.github.com>
2023-02-02 19:45:23 +0530

    my first app

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const { initializeApp,cert } = require('firebase-admin/app');
 const { getFirestore} = require('firebase-admin/firestore');
 
-const serviceAccount = require('./serviceAccount.json')
+const serviceAccount = require('./serviceAccount')
 
 // Initialize Firebase
 initializeApp({
